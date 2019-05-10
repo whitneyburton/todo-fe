0a983717ded5baffc431316cc2b87be6f737c5e0
Whitney Burton <whitneyburton9@gmail.com>
2019-05-10 17:48:15 -0600

    Add TodoForm component and styles

---
+++ src/containers/TodoForm/TodoForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const TodoForm = () => {
+  return (
+    <div className='TodoForm'>
+      <input placeholder='Title'></input>
+      <input placeholder='To-do Item'></input>
+    </div>
+  );
+}
+
+export default TodoForm;
