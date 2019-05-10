427e6b153b561ff5c996e65fef9e7f815b08db6d
Whitney Burton <whitneyburton9@gmail.com>
2019-05-10 17:18:46 -0600

    Add initial file structures and install redux, scss, jest, enzyme

---
+++ src/containers/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>To-do Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/main.scss';
+import App from './containers/App/App';
 import * as serviceWorker from './serviceWorker';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { rootReducer } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+const provider = (
+  <Provider store={store}>
+      <App />
+  </Provider>
+);
+
+ReactDOM.render(provider, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
