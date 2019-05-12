6fcc4daf886bc0a835b655f73df9a0aecdf02249
Whitney Burton <whitneyburton9@gmail.com>
2019-05-12 11:38:47 -0600

    Add dynamic api fetch call file

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-## To-do Tracker
+## Tracker
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/utils/api.js
@@ -0,0 +1,31 @@
+export const fetchData = async (path, method, data = null) => {
+  let options;
+  switch (method) {
+    case 'DELETE':
+      options = { method };
+      break
+    case 'GET':
+      options = data;
+      break
+    default:
+      options = {
+        method,
+        body: JSON.stringify(data),
+        headers: {
+          'Content-type': 'application/json'
+        }
+      };
+  };
+
+  const url = 'localhost:3001/api/v1' + path;
+  const response = await fetch(url, options);
+
+  if (response.status === 204) return 'success'
+
+  const json = await response.json();
+  if (response.ok) {
+    return json;
+  } else {
+    throw Error(json);
+  }
+}
