39b6969355f0ed5bdf29da1165167a201e9cb9dc
Whitney Burton <whitneyburton9@gmail.com>
2019-05-13 14:47:49 -0600

    Add App tests

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from '../containers/App/App';
+import { App, mapDispatchToProps } from '../containers/App/App';
+import { shallow } from 'enzyme';
+import { getTodos } from '../thunks/getTodos';
+jest.mock('../thunks/getTodos');
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  let wrapper;
+  let mockError;
+
+  beforeEach(() => {
+    mockError = '';
+
+    wrapper = shallow(
+      <App
+        getEvents={getTodos}
+        error={mockError}
+      />
+    )
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+})
