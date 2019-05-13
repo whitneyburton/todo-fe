15216c79bd8f809e9f6f8cea1ee14f24ec05c9cb
Whitney Burton <whitneyburton9@gmail.com>
2019-05-13 14:29:57 -0600

    Add useEffect and getTodos to App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getTodos } from '../../thunks/getTodos';
 import { TodoForm } from '../TodoForm/TodoForm';
 
-export const App = () => {
+export const App = ({ getTodos }) => {
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
     <div className='App'>
-      <h1>To-do Tracker</h1>
+      <h1>Tracker</h1>
       <TodoForm />
     </div>
   );
 };
 
-export default App;
+export const mapDispatchToProps = dispatch => ({
+  getTodos: () => dispatch(getTodos()),
+});
+
+export default connect(null, mapDispatchToProps)(App);
