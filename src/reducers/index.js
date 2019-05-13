7fe2b1e600937ac1f714f860aa1a2b63e125d45a
Whitney Burton <whitneyburton9@gmail.com>
2019-05-13 14:30:16 -0600

    Add todos, loading, and error to Redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,10 @@
 import { combineReducers } from 'redux';
+import { todosReducer } from './todosReducer/todosReducer';
+import { loadingReducer } from './loadingReducer/loadingReducer';
+import { errorReducer } from './errorReducer/errorReducer';
 
 export const rootReducer = combineReducers({
+  todos: todosReducer,
+  loading: loadingReducer,
+  error: errorReducer
 });
