9c84127985ba739e988c9fe6739d73a23c2a3de1
Whitney Burton <whitneyburton9@gmail.com>
2019-05-13 14:29:39 -0600

    Add setLoading and setError actions and tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,13 @@ export const setTodos = todos => ({
   type: 'SET_TODOS',
   todos
 });
+
+export const setLoading = loading => ({
+  type: 'SET_LOADING',
+  loading
+});
+
+export const setError = error => ({
+  type: 'SET_ERROR',
+  error
+});
--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -9,5 +9,25 @@ describe('actions', () => {
     };
     const result = actions.setTodos(mockTodos);
     expect(result).toEqual(expected);
-  })
+  });
+
+  it('should return a type of SET_LOADING with a boolean', () => {
+    const mockLoading = true;
+    const expected = {
+      type: 'SET_LOADING',
+      loading: mockLoading
+    };
+    const result = actions.setLoading(mockLoading);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return a type of SET_ERROR with an error message', () => {
+    const mockError = 'Error message';
+    const expected = {
+      type: 'SET_ERROR',
+      error: mockError
+    };
+    const result = actions.setError(mockError);
+    expect(result).toEqual(expected);
+  });
 });
