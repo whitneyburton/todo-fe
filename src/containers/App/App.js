5f6d144fa5f46e04553ca3a61255f01090264ae6
Whitney Burton <whitneyburton9@gmail.com>
2019-05-13 19:08:35 -0600

    Add TodosContainer to App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,18 +2,20 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { getTodos } from '../../thunks/getTodos';
 import { getItems } from '../../thunks/getItems';
-import { TodoForm } from '../TodoForm/TodoForm';
+import TodoForm from '../TodoForm/TodoForm';
+import TodoContainer from '../TodoContainer/TodoContainer';
 
 export const App = ({ getTodos, getItems }) => {
   useEffect(() => {
     getTodos();
-    getItems(2)
+    getItems(16)
   }, []);
 
   return (
     <div className='App'>
       <h1>Tracker</h1>
       <TodoForm />
+      <TodoContainer />
     </div>
   );
 };
