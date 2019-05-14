04e9aa6b55925a7cd4c54b3f64fe20335a11af32
Whitney Burton <whitneyburton9@gmail.com>
2019-05-13 19:07:25 -0600

    Connect TodoForm to Redux and useState for inputs

--- src/containers/TodoForm/TodoForm.js
+++ src/containers/TodoForm/TodoForm.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { postTodo } from '../../thunks/postTodo';
 
-export const TodoForm = () => {
+export const TodoForm = ({ postTodo }) => {
+  const [title, editTitle] = useState('To-do title');
+  const [item, editItem] = useState('To-do item');
+
+  const submitTodo = () => {
+    postTodo(title, 2);
+    // postItem(item, todoId)
+  }
+
   return (
     <div className='TodoForm'>
-      <input placeholder='Title'></input>
-      <input placeholder='To-do Item'></input>
+      <input placeholder={title} onChange={e => editTitle(e.target.value)} />
+      <input placeholder={item} onChange={e => editItem(e.target.value)} />
+      <button onClick={() => submitTodo()}>Submit</button>
     </div>
   );
-}
+};
 
-export default TodoForm;
+export const mapStateToProps = state => ({
+});
+
+export const mapDispatchToProps = dispatch => ({
+  postTodo: (todo, id) => dispatch(postTodo(todo, id))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
