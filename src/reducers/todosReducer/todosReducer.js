d8841b0dfd39e5d3088e703523ef51b9e27bc53c
Whitney Burton <whitneyburton9@gmail.com>
2019-05-13 19:08:12 -0600

    Add todosReducer

--- src/reducers/todosReducer/todosReducer.js
+++ src/reducers/todosReducer/todosReducer.js
@@ -2,6 +2,8 @@ export const todosReducer = (state = [], action) => {
   switch (action.type) {
     case 'SET_TODOS':
       return action.todos
+    case 'SET_TODO':
+      return [...state, action.todo];
     default:
       return state
   }
