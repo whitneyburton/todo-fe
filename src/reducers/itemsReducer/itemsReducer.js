85dd66617795553e1c617c1bf6f801c581848017
Whitney Burton <whitneyburton9@gmail.com>
2019-05-14 09:58:51 -0600

    Add SET_ITEM action to itemsReducer

--- src/reducers/itemsReducer/itemsReducer.js
+++ src/reducers/itemsReducer/itemsReducer.js
@@ -2,6 +2,8 @@ export const itemsReducer = (state = [], action) => {
   switch (action.type) {
     case 'SET_ITEMS':
       return action.items
+    case 'SET_ITEM':
+      return [...state, action.item];
     default:
       return state
   }
