df1d3517945f0dd247a6633142b88a7abf519f7b
Whitney Burton <whitneyburton9@gmail.com>
2019-05-14 09:56:52 -0600

    Add Todo card component

---
+++ src/components/TodoCard/TodoCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const TodoCard = ({ todo, todoItem }) => {
+  const toggleComplete = (id, bool) => {};
+
+  return (
+    <div className='TodoCard'>
+      <h1 className='todo-title'>{todo.title}</h1>
+      <button className={todoItem.done ? 'checked-icon' : 'unchecked-icon'} />
+      <p
+        className={todoItem.done ? 'item-complete' : 'item-incomplete'}
+        onClick={() => toggleComplete(todoItem.id)}
+      >
+        {todoItem.name}
+      </p>
+    </div>
+  );
+};
