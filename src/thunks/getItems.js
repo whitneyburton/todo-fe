26b2105a4eb614a6405350a2e466bb89c4c29058
Whitney Burton <whitneyburton9@gmail.com>
2019-05-14 09:59:17 -0600

    Clean item data in getItems thunk

--- src/thunks/getItems.js
+++ src/thunks/getItems.js
@@ -5,7 +5,10 @@ export const getItems = (todoId = '') => {
   return async dispatch => {
     try {
       dispatch(setLoading(true));
-      const items = await fetchData(`/todos/${todoId}/items`, 'GET');
+      const response = await fetchData(`/todos/${todoId}/items`, 'GET');
+      const items = response.map(item => {
+        return { name: item.name, done: item.done, todo_id: item.todo_id }
+      })
       dispatch(setItems(items));
       return items;
     } catch (error) {
