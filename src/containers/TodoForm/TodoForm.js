871a6543bf2e333b6294bd5d4788637a881bd632
Whitney Burton <whitneyburton9@gmail.com>
2019-05-14 09:58:34 -0600

    WIP: Add postTodo thunk to TodoForm and style

--- src/containers/TodoForm/TodoForm.js
+++ src/containers/TodoForm/TodoForm.js
@@ -1,21 +1,29 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { postTodo } from '../../thunks/postTodo';
+import { postItem } from '../../thunks/postItem';
 
-export const TodoForm = ({ postTodo }) => {
-  const [title, editTitle] = useState('To-do title');
-  const [item, editItem] = useState('To-do item');
+export const TodoForm = ({ postTodo, postItem }) => {
+  const [title, editTitle] = useState('Todo title');
+  const [item, editItem] = useState('Todo item');
 
-  const submitTodo = () => {
-    postTodo(title, 2);
-    // postItem(item, todoId)
+  const submitTodo = async () => {
+    const newTodo = await postTodo(title, 2);
+    const { id } = newTodo;
+    postItem(item, false, id)
+  }
+
+  const clearTodo = () => {
+    editTitle('')
+    editItem('')
   }
 
   return (
     <div className='TodoForm'>
-      <input placeholder={title} onChange={e => editTitle(e.target.value)} />
-      <input placeholder={item} onChange={e => editItem(e.target.value)} />
-      <button onClick={() => submitTodo()}>Submit</button>
+      <input className='title-input' placeholder={title} value={title} onClick={() => editTitle('')} onChange={e => editTitle(e.target.value)} />
+      <input className='item-input' placeholder={item} value={item} onClick={() => editItem('')} onChange={e => editItem(e.target.value)} />
+      <button className='submit-button' onClick={() => submitTodo()}>Submit Todo</button>
+      <button className='clear-button' onClick={() => clearTodo()}>Clear</button>
     </div>
   );
 };
@@ -24,7 +32,8 @@ export const mapStateToProps = state => ({
 });
 
 export const mapDispatchToProps = dispatch => ({
-  postTodo: (todo, id) => dispatch(postTodo(todo, id))
+  postTodo: (todo, id) => dispatch(postTodo(todo, id)),
+  postItem: (name, done, id) => dispatch(postItem(name, done, id))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
