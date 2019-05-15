f7d1a9544d9e397a58c6317bf51f86ddb44ce90e
Whitney Burton <whitneyburton9@gmail.com>
2019-05-15 10:49:28 -0600

    Add labels to inputs, clear inputs on submit, disable save btn

--- src/containers/TodoForm/TodoForm.js
+++ src/containers/TodoForm/TodoForm.js
@@ -2,15 +2,17 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { postTodo } from '../../thunks/postTodo';
 import { postItem } from '../../thunks/postItem';
+import { isNullOrUndefined } from 'util';
 
 export const TodoForm = ({ postTodo, postItem }) => {
-  const [title, editTitle] = useState('Todo title');
-  const [item, editItem] = useState('Todo item');
+  const [title, editTitle] = useState('');
+  const [item, editItem] = useState('');
 
   const submitTodo = async () => {
     const newTodo = await postTodo(title, 2);
     const { id } = newTodo;
     postItem(item, false, id)
+    clearTodo();
   }
 
   const clearTodo = () => {
@@ -20,20 +22,19 @@ export const TodoForm = ({ postTodo, postItem }) => {
 
   return (
     <div className='TodoForm'>
-      <input className='title-input' placeholder={title} value={title} onClick={() => editTitle('')} onChange={e => editTitle(e.target.value)} />
-      <input className='item-input' placeholder={item} value={item} onClick={() => editItem('')} onChange={e => editItem(e.target.value)} />
-      <button className='submit-button' onClick={() => submitTodo()}>Submit Todo</button>
+      <label for='title-input'>To-do title</label>
+      <input className='title-input' name='title-input' value={title} onClick={() => editTitle('')} onChange={e => editTitle(e.target.value)} />
+      <label for='item-input'>To-do item</label>
+      <input className='item-input' name='item-input' value={item} onClick={() => editItem('')} onChange={e => editItem(e.target.value)} />
+      <button className='submit-button' disabled={title.trim() === '' || item.trim() === ''} onClick={() => submitTodo()}>Submit Todo</button>
       <button className='clear-button' onClick={() => clearTodo()}>Clear</button>
     </div>
   );
 };
-
-export const mapStateToProps = state => ({
-});
 
 export const mapDispatchToProps = dispatch => ({
   postTodo: (todo, id) => dispatch(postTodo(todo, id)),
   postItem: (name, done, id) => dispatch(postItem(name, done, id))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
+export default connect(null, mapDispatchToProps)(TodoForm);
