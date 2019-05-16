cfba3f290cdf3c56d8bf13222c5df5af0cfe93ee
Whitney Burton <whitneyburton9@gmail.com>
2019-05-16 15:31:12 -0600

    Add userId to TodoCard and TodoForm and styles

--- src/containers/TodoCard/TodoCard.js
+++ src/containers/TodoCard/TodoCard.js
@@ -1,30 +1,33 @@
 import React from 'react';
-import ReactTooltip from 'react-tooltip'
+import ReactTooltip from 'react-tooltip';
 import { connect } from 'react-redux';
 import { updateItems } from '../../thunks/updateItems';
 import { deleteTodo } from '../../thunks/deleteTodo';
 
-export const TodoCard = ({ todo, todoItem, updateItems, deleteTodo, items }) => {
-
-
-  const toggleComplete = (todoId, bool, todoItem) => {
-    updateItems(todoId, bool, todoItem);
+export const TodoCard = ({
+  todo,
+  todoItem,
+  updateItems,
+  deleteTodo,
+  items,
+  userId
+}) => {
+  const toggleComplete = (todoId, bool) => {
+    updateItems(todoId, bool, userId);
   };
 
   const deleteCard = (todoId, itemId, items) => {
-    deleteTodo(todoId, itemId, items);
-  }
+    deleteTodo(todoId, itemId, items, userId);
+  };
 
-  const editCard = () => {
-
-  }
+  const editCard = () => {};
 
   return (
     <div className='TodoCard'>
       <h1 className='todo-title'>{todo.title}</h1>
       <div
         className='item-container'
-        onClick={() => toggleComplete(todo.id, todoItem.done, todoItem)}
+        onClick={() => toggleComplete(todo.id, todoItem.done)}
       >
         <button className={todoItem.done ? 'checked-icon' : 'unchecked-icon'} />
         <p className={todoItem.done ? 'item-complete' : 'item-incomplete'}>
@@ -32,23 +35,42 @@ export const TodoCard = ({ todo, todoItem, updateItems, deleteTodo, items }) => 
         </p>
       </div>
       <div className='buttons-container'>
-        <button data-tip data-for="edit-tip" className='edit-todo' onClick={() => editCard(todo.id, todoItem.id, items)} />
-        <ReactTooltip id='edit-tip' effect='solid'>Edit</ReactTooltip>
-        <button data-tip data-for="delete-tip" className='delete-todo' onClick={() => deleteCard(todo.id, todoItem.id, items)}/>
-        <ReactTooltip id='delete-tip' effect='solid'>Delete</ReactTooltip>
+        <button
+          data-tip
+          data-for='edit-tip'
+          className='edit-todo'
+          onClick={() => editCard(todo.id, todoItem.id, items)}
+        />
+        <ReactTooltip id='edit-tip' effect='solid'>
+          Edit
+        </ReactTooltip>
+        <button
+          data-tip
+          data-for='delete-tip'
+          className='delete-todo'
+          onClick={() => deleteCard(todo.id, todoItem.id, items)}
+        />
+        <ReactTooltip id='delete-tip' effect='solid'>
+          Delete
+        </ReactTooltip>
       </div>
     </div>
-  )
+  );
 };
 
 export const mapStateToProps = state => ({
-  items: state.items
+  items: state.items,
+  userId: state.userId
 });
 
 export const mapDispatchToProps = dispatch => ({
-  updateItems: (todoId, itemId, bool, todoItem) =>
-    dispatch(updateItems(todoId, itemId, bool, todoItem)),
-  deleteTodo: (todoId, itemId, items) => dispatch(deleteTodo(todoId, itemId, items))
+  updateItems: (todoId, itemId, bool, userId) =>
+    dispatch(updateItems(todoId, itemId, bool, userId)),
+  deleteTodo: (todoId, itemId, items, userId) =>
+    dispatch(deleteTodo(todoId, itemId, items, userId))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(TodoCard);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoCard);
--- src/containers/TodoForm/TodoForm.js
+++ src/containers/TodoForm/TodoForm.js
@@ -2,39 +2,66 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { postTodo } from '../../thunks/postTodo';
 import { postItem } from '../../thunks/postItem';
-import { isNullOrUndefined } from 'util';
 
-export const TodoForm = ({ postTodo, postItem }) => {
+export const TodoForm = ({ postTodo, postItem, userId }) => {
   const [title, editTitle] = useState('');
   const [item, editItem] = useState('');
 
   const submitTodo = async () => {
-    const newTodo = await postTodo(title, 2);
+    const newTodo = await postTodo(title, userId);
     const { id } = newTodo;
-    postItem(item, false, id)
+    postItem(item, false, id, userId);
     clearTodo();
-  }
+  };
 
   const clearTodo = () => {
-    editTitle('')
-    editItem('')
-  }
+    editTitle('');
+    editItem('');
+  };
 
   return (
     <div className='TodoForm'>
       <label for='title-input'>To-do title</label>
-      <input className='title-input' name='title-input' value={title} onClick={() => editTitle('')} onChange={e => editTitle(e.target.value)} />
+      <input
+        className='title-input'
+        name='title-input'
+        value={title}
+        onClick={() => editTitle('')}
+        onChange={e => editTitle(e.target.value)}
+      />
       <label for='item-input'>To-do item</label>
-      <input className='item-input' name='item-input' value={item} onClick={() => editItem('')} onChange={e => editItem(e.target.value)} />
-      <button className='submit-button' disabled={title.trim() === '' || item.trim() === ''} onClick={() => submitTodo()}>Submit Todo</button>
-      <button className='clear-button' onClick={() => clearTodo()}>Clear</button>
+      <input
+        className='item-input'
+        name='item-input'
+        value={item}
+        onClick={() => editItem('')}
+        onChange={e => editItem(e.target.value)}
+      />
+      <button
+        className='submit-button'
+        disabled={title.trim() === '' || item.trim() === ''}
+        onClick={() => submitTodo()}
+      >
+        Submit Todo
+      </button>
+      <button className='clear-button' onClick={() => clearTodo()}>
+        Clear
+      </button>
     </div>
   );
 };
 
-export const mapDispatchToProps = dispatch => ({
-  postTodo: (todo, id) => dispatch(postTodo(todo, id)),
-  postItem: (name, done, id) => dispatch(postItem(name, done, id))
+export const mapStateToProps = state => ({
+  userId: state.userId
 });
 
-export default connect(null, mapDispatchToProps)(TodoForm);
+export const mapDispatchToProps = dispatch => ({
+  postTodo: (todo, id) => dispatch(postTodo(todo, id)),
+  postItem: (name, done, id, userId) =>
+    dispatch(postItem(name, done, id, userId))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoForm);
