41edda6602f3c21f4cb31777833e0ff256a4bb82
Whitney Burton <whitneyburton9@gmail.com>
2019-05-16 15:29:57 -0600

    Add empty array to useEffect and retrieveUser fn()

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,25 +1,41 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { getTodos } from '../../thunks/getTodos';
+import { getUser } from '../../thunks/getUser';
 import TodoForm from '../TodoForm/TodoForm';
 import TodoContainer from '../TodoContainer/TodoContainer';
 
-export const App = ({ getTodos }) => {
+export const App = ({ getTodos, getUser, userId }) => {
   useEffect(() => {
-    getTodos();
-  });
+    retrieveUser();
+  }, []);
+  
+  const retrieveUser = async () => {
+    const id = await getUser('whitney@gmail.com', '123123');
+    getTodos(id);
+  }
 
   return (
+    userId ? 
     <div className='App'>
       <h1 className='tracker-title'>TRACKER</h1>
       <TodoForm />
       <TodoContainer />
-    </div>
+      </div> :
+      <div className='App'>
+        <h1 className='tracker-title'>TRACKER</h1>
+        <h1 className='signin-prompt'>Please sign in or register!</h1>
+      </div>
   );
 };
 
-export const mapDispatchToProps = dispatch => ({
-  getTodos: () => dispatch(getTodos())
+export const mapStateToProps = state => ({
+  userId: state.userId
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export const mapDispatchToProps = dispatch => ({
+  getTodos: id => dispatch(getTodos(id)),
+  getUser: (email, password) => dispatch(getUser(email, password))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
