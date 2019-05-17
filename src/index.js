21b6f695a265ce71f8870d2d0a3f0b67d91f826c
Whitney Burton <whitneyburton9@gmail.com>
2019-05-16 23:19:28 -0600

    Add react-router

--- src/index.js
+++ src/index.js
@@ -8,12 +8,16 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { rootReducer } from './reducers';
+import { BrowserRouter } from 'react-router-dom';
+
 
 const devTools = composeWithDevTools(applyMiddleware(thunk));
 const store = createStore(rootReducer, devTools);
 const provider = (
   <Provider store={store}>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
   </Provider>
 );
 
