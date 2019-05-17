205ab2639b5efabe03521620d52524e2c047ebda
Whitney Burton <whitneyburton9@gmail.com>
2019-05-17 03:19:39 -0600

    WIP: Begin logoutUser thunk

--- src/thunks/logoutUser.js
+++ src/thunks/logoutUser.js
@@ -1,12 +1,14 @@
-import { setLoading, setError, setUser } from '../actions';
+import { setLoading, setError, setUser, setTodos, setItems } from '../actions';
 import { fetchData } from '../utils/api';
 
 export const logoutUser = id => {
   return async dispatch => {
     try {
       dispatch(setLoading(true));
-      await fetchData(`/sessions?id=${id}`, 'DELETE');
+      // await fetchData(`/sessions?id=${id}`, 'DELETE');
       dispatch(setUser(''));
+      dispatch(setTodos([]));
+      dispatch(setItems([]));
       dispatch(setError(''));
     } catch (error) {
       dispatch(setError(error.message));
