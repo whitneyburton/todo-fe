a56a0a69eb6bba7a2946ec2a20e982ef6f1f128f
Whitney Burton <whitneyburton9@gmail.com>
2019-05-17 01:35:07 -0600

    Add done icon to header

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -6,19 +6,21 @@ import TodoForm from '../TodoForm/TodoForm';
 import TodoContainer from '../TodoContainer/TodoContainer';
 import { Welcome } from '../../components/Welcome/Welcome';
 import { withRouter, Route, Switch } from 'react-router-dom';
+import doneicon from '../../images/done-icon.svg';
 
-export const App = ({ userId, error, location,  history }) => {
+export const App = ({ userId, error, location, history }) => {
   useEffect(() => {
     !userId && history.replace('/');
-  })
+  });
 
   return (
     <div className='App'>
-      <h1 className='tracker-title'>TRACKER</h1>
+      <h1 className='tracker-title'>
+        TRACKER
+        <img className='done-icon' src={doneicon} alt='check icon' />
+      </h1>
       <Switch>
-        <Route exact path='/' render={() => (
-          <Welcome history={history} />
-        )}/>
+        <Route exact path='/' render={() => <Welcome history={history} />} />
         <Route
           path='/home'
           render={() => (
@@ -30,7 +32,7 @@ export const App = ({ userId, error, location,  history }) => {
         />
       </Switch>
     </div>
-  )
+  );
 };
 
 export const mapStateToProps = state => ({
